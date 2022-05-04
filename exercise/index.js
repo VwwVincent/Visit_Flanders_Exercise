e59671b1bb437981417aca5a2493492de0c87b1c
Vanwetswinkel Vincent <Vincent.Vanwetswinkel@euroshoe.com>
2022-05-05 00:30:28 +0200

    begin het te begrijpen, enkel het subtype icon nog bepalen en aanmaken

--- exercise/index.js
+++ exercise/index.js
@@ -17,8 +17,33 @@
     const newRouteElement = document
       .querySelector("#route-template")
       .content.cloneNode(true);
-    const name = newRouteElement.querySelector(".name");
-    name.innerText = route.name;
+    const name = (newRouteElement.querySelector(".name").innerText =
+      route.name);
+    const description = (newRouteElement.querySelector(
+      ".description"
+    ).innerText = route.accessibility_description);
+    const location = (newRouteElement.querySelector(".location").innerText =
+      route.city_name);
+
+    if (route.website) {
+      const websiteLink = (newRouteElement.querySelector(".website").href =
+        route.website);
+      const websiteText = (newRouteElement.querySelector(".website").innerText =
+        route.website);
+    } else {
+      const websiteHide = (newRouteElement.querySelector(".site").innerText =
+        "");
+    }
+
+    if (route.email) {
+      const emailLink = (newRouteElement.querySelector(
+        ".email"
+      ).href = `mailto:${route.email}`);
+      const emailText = (newRouteElement.querySelector(".email").innerText =
+        route.email);
+    } else {
+      const emailHide = (newRouteElement.querySelector(".mail").innerText = "");
+    }
 
     const routesElement = document.querySelector("#routes");
     routesElement.appendChild(newRouteElement);
